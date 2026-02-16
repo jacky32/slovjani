52629852d0285c10eae035da5c7f64f5506f7c25
jacky32 <4532511+jacky32@users.noreply.github.com>
2026-02-16 16:14:51 +0100

    Rename posts

--- app/views/admin/posts/index.html.php
+++ app/views/admin/posts/index.html.php
@@ -1,6 +1,6 @@
 <?= $this->renderPartial("admin/posts/_left_pane", ['posts' => $posts, 'errors' => isset($errors) ? $errors : []]) ?>
 <section id="rightpane">
-  index stránka příspěvků
+  index stránka zpráv
   <br><br>
   <a href='/admin/posts/new' class='button'><?= t("posts.new.title") ?></a>
 </section>
--- app/views/posts/index.html.php
+++ app/views/posts/index.html.php
@@ -1,6 +1,6 @@
 <?= $this->renderPartial("posts/_left_pane", ['posts' => $posts, 'errors' => isset($errors) ? $errors : []]) ?>
 <section id="rightpane">
-  index stránka příspěvků
+  index stránka zpráv
   <br><br>
   <!-- <a href='/posts/new' class='button'><?= t("posts.new.title") ?></a> -->
 </section>
