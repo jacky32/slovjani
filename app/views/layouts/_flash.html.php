28beec668b4895f78805a857ee280ce48710ef20
jacky32 <4532511+jacky32@users.noreply.github.com>
2026-03-01 17:35:41 +0100

    Update flash css

--- app/views/layouts/_flash.html.php
+++ app/views/layouts/_flash.html.php
@@ -1,16 +1,27 @@
-<?php
-if (FlashManager::hasFlashes()) {
-  $flashes = FlashManager::getFlashes();
-  foreach ($flashes as $type => $messages) {
-    echo '<div class="flash">';
-    echo '  <div class="alert-' . $type . '">';
-    echo '    <button type="button">';
-    echo '      <span><i></i></span>';
-    echo '    </button>';
-    foreach ($messages as $message) {
-      echo "<span>$message</span><br />";
+<?php if (FlashManager::hasFlashes()) : ?>
+  <?php $flashes = FlashManager::getFlashes(); ?>
+  <div class="flash-container">
+    <?php foreach ($flashes as $type => $messages) : ?>
+      <?php foreach ($messages as $message) : ?>
+        <div class="flash alert-<?= htmlspecialchars($type) ?>" role="alert">
+          <p><?= htmlspecialchars($message) ?></p>
+          <button type="button" class="close" aria-label="Zavřít"><span></span></button>
+        </div>
+      <?php endforeach; ?>
+    <?php endforeach; ?>
+  </div>
+  <script>
+    {
+      const container = document.querySelector('.flash-container');
+      if (container) {
+        container.querySelectorAll('.close').forEach(btn => {
+          btn.addEventListener('click', () => {
+            btn.closest('.flash')?.remove();
+            if (!container.querySelector('.flash')) container.remove();
+          });
+        });
+        setTimeout(() => container.remove(), 5000);
+      }
     }
-    echo '  </div>';
-    echo '</div>';
-  }
-}
+  </script>
+<?php endif; ?>
