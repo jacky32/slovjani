b2a940adb90081cb45f73f98ec30ca373d928773
jacky32 <4532511+jacky32@users.noreply.github.com>
2026-03-02 16:03:30 +0100

    Fix pagination in show/edit actions in left_pane

--- lib/active_model/relations/pagination.php
+++ lib/active_model/relations/pagination.php
@@ -14,6 +14,13 @@ class Pagination
   {
     $this->total_count = $query->count();
     $this->total_pages = ceil($this->total_count / $this->per_page) ?: 1;
+
+    if ($start_id !== null && $current_page === null) {
+      $ids = $query->pluck('id');
+      $position = array_search($start_id, $ids);
+      $current_page = $position !== false ? (int) ceil(($position + 1) / $this->per_page) : 1;
+    }
+
     $this->current_page = $current_page ? max(1, min($current_page, $this->total_pages)) : 1;
     $this->previous_page = $this->current_page > 1 ? $this->current_page - 1 : null;
     $this->next_page = ($this->current_page * $this->per_page) < $this->total_count ? $this->current_page + 1 : null;
